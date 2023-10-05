b21e709de7647bfd4960a3c424567eeb9e921d59
Mohammod Ali <mohammodali651@gmail.com>
2023-10-05 16:25:43 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from './Pages/Footer/Footer'
 import Home from './Pages/Home/Home'
+import Navbar from './Pages/Navbar/Navbar'
 
 function App() {
  
   return (
     <>
+      <Navbar></Navbar>
       <Home></Home>
       <Footer></Footer>
     </>
--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Navbar.css'
+import navLogo from '../../assets/RIDE EVEE Logo 1.png'
+import navPic from '../../assets/nav pic.png'
+import navBtn from '../../assets/nav btn.png'
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='navbar'>
+            <div>
+                <img src={navLogo} alt="Logo" />
+            </div>
+            <div>
+                <img src={navPic} alt="navPic" />
+            </div>
+            <div className='navBtn'>
+                <button>Customer Sign In</button>
+                <button style={{backgroundColor: '#38B000', color: 'white'}}>Vendor Sign In</button>
+                <img style={{cursor: 'pointer'}} src={navBtn} alt="navBtn" />
+            </div>
         </div>
     );
 };
