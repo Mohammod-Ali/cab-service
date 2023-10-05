c04b93231526783322f08086d0f325431d3f2267
Mohammod Ali <mohammodali651@gmail.com>
2023-10-05 12:55:34 +0600

    why choose yatri car rental section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -56,7 +56,21 @@ const Home = () => {
             </div>
         </div>
 
-        
+        {/* Why Choose Yatri Car Rental? */}
+        <div style={{marginTop: '90px', padding: '30px'}}>
+            <h1  className='text-clr'>Why Choose Yatri Car Rental?</h1>
+
+            <p><span className='text-bold'>*</span> Yatri Car Rental has been a trusted digital platform for booking local and outstation car rental and taxi services since 2014. Our primary goal is to offer our clients an exceptional car rental and cab booking experience, and we take great pride in our commitment to safety, reliability, and experienced drivers. These qualities are evident across all our car rental services, making us the preferred choice for those seeking rental cars and taxi services in INDIA.</p>
+
+            <p><span className='text-bold'>* Service You Can Trust -</span> At Yatri Car Rental, we are dedicated to providing dependable services in INDIA. We guarantee that we won't cancel any bookings without advance notification, and our rental cars are meticulously maintained to ensure a seamless and stress-free journey for our clients.</p>
+
+            <p><span className='text-bold'>* Safety is Our Priority -</span> Safety and comfort are paramount at Yatri Car Rental in INDIA. We exclusively hire experienced and skilled drivers who possess in-depth knowledge of the city's roads and traffic regulations. Our drivers are committed to delivering excellent customer service and ensuring a comfortable and stress-free travel experience for passengers.</p>
+
+            <p><span className='text-bold'>* Expert Chauffeurs at Your Service -</span> Yatri Car Rental takes pride in selecting only the most skilled, knowledgeable, and courteous drivers to enhance the convenience and satisfaction of our clients.</p>
+
+            <p><span className='text-bold'>* Extensive Industry Experience -</span> With over 10 years of industry expertise, Yatri Car Rental is your go-to choice for premium car hire services in Lucknow, Uttar Pradesh.</p>
+
+        </div>
         </>
        
     );
